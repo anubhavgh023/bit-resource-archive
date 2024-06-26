e56510db070e81d1e07d5cb4244625d8c7c71ea3
Anubhav <anubhavsamanta023@gmail.com>
2024-06-26 21:33:40 +0530

    conditional rendering fixed

--- src/app/dashboard/[...path]/page.tsx
+++ src/app/dashboard/[...path]/page.tsx
@@ -1,46 +1,64 @@
 "use client";
 import { useState } from "react";
 import SemesterCard from "@/components/SemesterCard";
-import { usePathname,useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import axios from "axios";
+import QuestionPaperDisplay from "@/components/QuestionPaperDisplay";
 
 export default function DashboardPage() {
   const [selectedSemester, setSelectedSemester] = useState<string>("");
+  const [questionPaperData, setQuestionPaperData] = useState<
+    { course: string; downloadLink: string }[]
+  >([]);
   const semesterCount = ["1", "2", "3", "4", "5", "6"];
 
-  const router = useRouter();
-
+  // get query details
   const pathName = usePathname();
-
-  const urlSegments = pathName.split('/');
+  const urlSegments = pathName.split("/");
   const course = urlSegments[2];
   const year = urlSegments[3];
 
+  // handle click
   async function handleClick(semester: string) {
     setSelectedSemester(semester);
 
-    const res = await axios.post(`/api/questionPapers/${course}/${year}/semester_${semester}`, {
-      course,
-      year,
-      semester
-    })
-    console.log(JSON.stringify(res.data.data));
+    try {
+      const res = await axios.post(
+        `/api/questionPapers/${course}/${year}/semester_${semester}`,
+        {
+          course,
+          year,
+          semester,
+        }
+      );
 
-    router.push(`/questionPapers`);
+      console.log(res.data.data);
+
+      // set the question paper data for rendering
+      setQuestionPaperData(res.data.data[`semester_${semester}`]);
+    } catch (error) {
+      console.error("Error fetching question papers:", error);
+    }
   }
 
+  console.log(questionPaperData);
 
+  // --- conditional rendering of dashboard ---
   return (
-    <div className="border border-slate-600 rounded-sm grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-full justify-between items-center p-10">
-      {semesterCount.map((semester) => {
-        return (
+    // question papers 
+    <div className="border border-slate-600 rounded-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-full justify-between items-center p-10">
+      {questionPaperData.length > 0 ? (
+        <QuestionPaperDisplay data={questionPaperData} />
+      ) : (
+        // semester cards
+        semesterCount.map((semester) => (
           <SemesterCard
             key={semester}
             semester={semester}
-            onClick={handleClick}
-          ></SemesterCard>
-        );
-      })}
+            onClick={() => handleClick(semester)}
+          />
+        ))
+      )}
     </div>
   );
 }
