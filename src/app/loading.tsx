d6fdeb5af9de6194f564896c6336f88d74140cc3
Anubhav <anubhavsamanta023@gmail.com>
2024-06-28 05:04:21 +0530

    simplified loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,8 @@
-import { Progress } from "@/components/ui/progress";
-
 export default function LoadingPage() {
   return (
     <div className="border border-slate-600 rounded-sm flex flex-col w-full justify-center items-center p-10 gap-2">
-      <span className="font-semibold">Loading...</span>
-      <div className="w-32">
-        <Progress value={70}></Progress>
+      <div className="flex gap-4">
+        <span className="font-semibold">Loading...</span>
       </div>
     </div>
   );
