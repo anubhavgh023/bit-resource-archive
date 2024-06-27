6cbca002b82976e297afdd384ecea5383b5351cc
Anubhav <anubhavsamanta023@gmail.com>
2024-06-27 13:28:21 +0530

    loading skeleton added

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,12 @@
+import { Progress } from "@/components/ui/progress";
+
 export default function LoadingPage() {
-    return (
-        <div>
-            Loading...
-        </div>
-    )
+  return (
+    <div className="border border-slate-600 rounded-sm flex flex-col w-full justify-center items-center p-10 gap-2">
+      <span className="font-semibold">Loading...</span>
+      <div className="w-32">
+        <Progress value={70}></Progress>
+      </div>
+    </div>
+  );
 }
