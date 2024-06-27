83c2000b3045682cc880fa33960ca16f721b20d4
Anubhav <anubhavsamanta023@gmail.com>
2024-06-27 18:48:33 +0530

    vercel speed-insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SideBar from "@/components/SideBar";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
