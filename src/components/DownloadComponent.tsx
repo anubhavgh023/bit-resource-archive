a40d8477dd9b8951f43db79e1ab3d585f9dae69c
Anubhav <anubhavsamanta023@gmail.com>
2024-06-27 14:34:04 +0530

    Made it responsive

--- src/components/DownloadComponent.tsx
+++ src/components/DownloadComponent.tsx
@@ -13,14 +13,16 @@ export default function DownloadComponentCard({
   }
 
   return (
-    <div className="flex justify-around p-2">
-      <Button variant={"outline"} className="w-3/5 hover:bg-transparent border-slate-700">
+    <div className="flex flex-col sm:flex-row justify-around p-2 gap-2">
+      <Button
+        variant={"outline"}
+        className="w-full sm:w-3/5 hover:bg-transparent border-slate-700 text-xs sm:text-sm md:text-base">
         {courseName}
       </Button>
-      <a href={downloadLink}>
+      <a href={downloadLink} className="w-full sm:w-auto">
         <Button
           variant={"outline"}
-          className="ring-1 ring-blue-400 hover:ring-2 active:scale-95 transition delay-15"
+          className="w-full sm:w-auto ring-1 ring-blue-400 hover:ring-2 active:scale-95 transition delay-15"
           onClick={handleClick}
         >
           Download
