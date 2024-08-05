23bb52cf304da23e394451c2baea38903e4505f7
Anubhav <anubhavsamanta023@gmail.com>
2024-08-05 20:29:16 +0530

    vercel analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SideBar from "@/components/SideBar";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import TitleBar from "@/components/TitleBar";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
+        <div className="flex justify-center items-center py-5">
+          <TitleBar></TitleBar>
+        </div>
         <div className="flex flex-col md:flex-row lg:flex-row gap-10 justify-between p-10">
           <SideBar></SideBar>
+          <Analytics></Analytics>
           {children}
         </div>
       </body>
