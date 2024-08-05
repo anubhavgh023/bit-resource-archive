d530f2aa5b0d5bb8991737abf0cd8e75822d67a5
Anubhav <anubhavsamanta023@gmail.com>
2024-08-05 21:06:10 +0530

    [fixed]: spelling corrected Lalpur

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -1,7 +1,7 @@
 export default function TitleBar(){
     return (
         <div className="p-5 text-2xl border rounded-sm border-slate-700">
-            Birla Institute Of Technology, Lalput Extention
+            Birla Institute Of Technology, Lalpur Extention
         </div>
     )
 }
