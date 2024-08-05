f87a76015d52d500b98c0fbeb57f83babbed1a2e
Anubhav <anubhavsamanta023@gmail.com>
2024-08-05 20:31:02 +0530

    [added]: new TitleBar component

---
+++ src/components/TitleBar.tsx
@@ -0,0 +1,7 @@
+export default function TitleBar(){
+    return (
+        <div className="p-5 text-2xl border rounded-sm border-slate-700">
+            Birla Institute Of Technology, Lalput Extention
+        </div>
+    )
+}
