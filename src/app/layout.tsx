b79c1d138242bff1a5c28ae0f187f2cae91e3931
Akash kumar <Aakashkkr04@gmail.com>
2024-08-08 23:36:33 +0530

    [changed]: change ui design
    [fixed]: added navigation to home in Titlebar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import SideBar from "@/components/SideBar";
 import TitleBar from "@/components/TitleBar";
 import { Analytics } from "@vercel/analytics/react";
+import Footer from "@/components/Footer";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,15 +20,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>
-        <div className="flex justify-center items-center py-5">
-          <TitleBar></TitleBar>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
+        <Link href="/" className="flex justify-center items-center py-5">
+          <TitleBar />
+        </Link>
+        <div className="flex-grow flex flex-col md:flex-row lg:flex-row gap-10 justify-between p-10">
+          <SideBar />
+          <main className="flex-grow">{children}</main>
+          <Analytics />
         </div>
-        <div className="flex flex-col md:flex-row lg:flex-row gap-10 justify-between p-10">
-          <SideBar></SideBar>
-          <Analytics></Analytics>
-          {children}
-        </div>
+        <footer className="mt-auto w-full px-10">
+          <Footer />
+        </footer>
       </body>
     </html>
   );
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function LoadingPage() {
   return (
-    <div className="border border-slate-600 rounded-sm flex flex-col w-full justify-center items-center p-10 gap-2">
+    <div className="rounded-sm flex flex-col w-full justify-center items-center p-10 gap-2">
       <div className="flex gap-4">
         <span className="font-semibold">Loading...</span>
       </div>
